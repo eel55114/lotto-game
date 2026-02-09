240ceca0187b2e396718a4dcf8f63d98a481939f
eel55114 <eel55114@gmail.com>
2026-02-09 15:25:55 +0900

    화면 번호 표시 기능 추가

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ lotto_num = range(1, 46)
 
 
 def buttonClick():
-    print(random.sample(lotto_num, 6))
+    label = tkinter.Label(window, text=str(random.sample(lotto_num, 6)))
+    label.pack()
 
 
 window = tkinter.Tk()
